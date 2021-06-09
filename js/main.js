2942dc92a2ee33cb6ec0d7beb3f8787dd7e4dcfd
John Ryan Roxas <jroxas@jway.com>
2021-06-10 01:22:49 +0800

    CSS Update

--- js/main.js
+++ js/main.js
@@ -289,5 +289,26 @@ $(function(){
         
     });
 
+
+    // Product Individual Page
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
+    $('.product-details-btn button.btn, .featured-item-box button.btn').click(function () {
+        $(this).toggleClass("btn-clicked");
+    });
+
 });
 
